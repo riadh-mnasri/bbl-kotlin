3528836e5c27e1debae600a569b859eef12d4087
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-03-22 10:45:57 +0100

    Iteration 5 - part 3 Elvis operator

--- src/main/kotlin/com/bbl/samples/Book.kt
+++ src/main/kotlin/com/bbl/samples/Book.kt
@@ -2,7 +2,11 @@ package com.bbl.samples
 
 import kotlin.random.Random
 
-data class Book(val isbn: String, val name: String) {
+data class Book(val isbn: String, val name: String, val price: Int=-1) {
+    fun isExpensive(): String {
+        return if(price > 40) "Expensive" else "Good to buy"
+    }
+
     var isBookAvailable: Boolean
     get() = Random.nextBoolean()
     set(value){
--- src/main/kotlin/com/bbl/samples/Demo.kt
+++ src/main/kotlin/com/bbl/samples/Demo.kt
@@ -2,12 +2,20 @@ package com.bbl.samples
 
 fun main(){
     // Variable declaration, inference type, instanciation classe
-   val bookKotlin = Book("ISBNTEST1", "Programming Kotlin")
+   val bookKotlin = Book("ISBNTEST1", "Programming Kotlin", 30)
+   val bookScala = Book("ISBNTEST2", "Programming Scala", 45)
    val library = Library(listOf(bookKotlin))
     // Null Safety
-   val foundBook = library.findBookByIsbn("ISBNTEST1")
+   // Properties
    val isBookAvailable = bookKotlin.isBookAvailable
-    println("isBookAvailable :: $isBookAvailable")
+   //println("isBookAvailable :: $isBookAvailable"
+    val bookPriceEval = bookKotlin.isExpensive()
+    //println("Book price eval :: $bookKotlin.name $bookPriceEval")
+   val foundBook = library.findBookByIsbn("ISBNTEST1")
+   val bookToRead = library.findBookByIsbn("TOTO") ?: Book("ISBN3","Domain Driver Design")
+    println("found book >> $bookToRead")
+
+
 
 
 }
