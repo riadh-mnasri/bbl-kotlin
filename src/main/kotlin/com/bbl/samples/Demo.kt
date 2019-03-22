6bd439b305bd94cc87e6a14c7beb8a0552ae60ad
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-03-22 11:12:31 +0100

    Iteration 5 - part 3 - Extension function

--- src/main/kotlin/com/bbl/samples/Demo.kt
+++ src/main/kotlin/com/bbl/samples/Demo.kt
@@ -7,13 +7,15 @@ fun main(){
    val library = Library(listOf(bookKotlin))
     // Null Safety
    // Properties
-   val isBookAvailable = bookKotlin.isBookAvailable
+   //val isBookAvailable = bookKotlin.isBookAvailable
    //println("isBookAvailable :: $isBookAvailable"
     val bookPriceEval = bookKotlin.isExpensive()
     //println("Book price eval :: $bookKotlin.name $bookPriceEval")
    val foundBook = library.findBookByIsbn("ISBNTEST1")
    val bookToRead = library.findBookByIsbn("TOTO") ?: Book("ISBN3","Domain Driver Design")
     //println("found book >> $bookToRead")
+    val kotlinBookStarsNumber = bookKotlin.rate(5)
+    println("kotlinBookStarsNumber >> $kotlinBookStarsNumber")
 
 
 
