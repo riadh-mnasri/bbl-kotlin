96a8b67b80f42c116a6fbf0ea975c301f58d86cd
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-03-26 12:54:31 +0100

    Polish

--- src/main/kotlin/com/bbl/samples/Library.kt
+++ src/main/kotlin/com/bbl/samples/Library.kt
@@ -4,6 +4,6 @@ data class Library(val books: List<Book>) {
     fun findBookByIsbn(isbn: String): BookResult {
        val result = books.firstOrNull { it.isbn == isbn }
        return  if (result == null) BookResult.BookNotFound(isbn)
-        else BookResult.BookFound(result)
+       else BookResult.BookFound(result)
     }
 }
