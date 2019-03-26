c6d3f296ae0db051442e1b9c276cd8e9c7f427de
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-03-26 12:59:36 +0100

    Iteration 8 - RAZ

--- src/main/kotlin/com/bbl/samples/Library.kt
+++
@@ -1,9 +0,0 @@
-package com.bbl.samples
-
-data class Library(val books: List<Book>) {
-    fun findBookByIsbn(isbn: String): BookResult {
-       val result = books.firstOrNull { it.isbn == isbn }
-       return  if (result == null) BookResult.BookNotFound(isbn)
-       else BookResult.BookFound(result)
-    }
-}
