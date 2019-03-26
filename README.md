8423bfbd6d09407a9194a4b0fa509dc259ef9ec5
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-03-26 23:41:57 +0100

    Polish

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BBL - Kotlin sample project for demo usage
+kotlinc -script script.kts
--- script.kts
+++ script.kts
@@ -1,2 +1,2 @@
 fun greet(name: String, message: String)= println("$message $name")
-greet("Enedis", "Hello")
+greet("World", "Hello")
